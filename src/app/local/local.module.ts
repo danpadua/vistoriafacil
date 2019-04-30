08c3566d0f1236c3683f116c6f0a0d7a3ef7f9d3
Danilo <danilo.oliveira@siagri.com.br>
2019-04-30 16:26:39 -0300

    Adicionando rotas e tabs

--- src/app/local/local.module.ts
+++ src/app/local/local.module.ts
@@ -2,21 +2,34 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { LocalPage } from './local.page';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LocalPage,
+    children: [
+      {
+        path: 'dados',
+        loadChildren: './vistoria/vistoria.module#VistoriaPageModule'
+      },
+      {
+        path: 'comodos',
+        loadChildren: './vistoria/vistoria.module#VistoriaPageModule'
+      }
+    ]
+  }
+] 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: LocalPage
-      }
-    ])
+    RouterModule.forChild(routes)
   ],
   declarations: [LocalPage]
 })
--- src/app/local/local.page.ts
+++ src/app/local/local.page.ts
@@ -1,38 +1,8 @@
 import { Component } from '@angular/core';
-import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'local.page.html',
   styleUrls: ['local.page.scss'],
 })
-export class LocalPage {
-
-  constructor(public navCtrl: LocalController, public geolocation: Geolocation) {
-
-  }
-
-  ionViewDidLoad(){
-    this.loadMap();
-  }
-
-  loadMap(){
-
-    this.geolocation.getCurrentPosition().then((position) => {
-
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
-    }, (err) => {
-      console.log(err);
-    });
-
-  }
-}
+export class LocalPage { }
